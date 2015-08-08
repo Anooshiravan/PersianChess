69a10f35713a33e76d3995e2817d0c2146451500
Anoosh Ahmadi <aahmadi@schubergphilis.com>
2015-08-09 00:04:20 +0200

    .

--- js/adapter.js
+++ js/adapter.js
@@ -460,6 +460,7 @@ function AlertEndGame() {
 
 
 function Help() {
+    LoadAudio();
     lla.play( welcome );
     var go2web = confirm("Version 1.2.2\r\nPersian Chess is invented and programmed by:\r\nAnooshiravan Ahmadi\r\nDo you want to go to www.PersianChess.com for the detailed game rules?");
     if (go2web == true) {
